05821d0010230adcc96fdffc19fab39fd2f59bbf
Alex M. Maldonado <aalexmmaldonado@gmail.com>
2023-08-16 14:11:32 -0400

    Fix actions Python env

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 ./cleanup-lib-refs.sh
 
 jupyter-book clean ./book
-jupyter-book build ./book --builder linkcheck
+# jupyter-book build ./book --builder linkcheck
 jupyter-book build ./book
 ./cleanup_printing.py
 cp -rf ./book/slides/revealjs ./book/_build/html/slides/
