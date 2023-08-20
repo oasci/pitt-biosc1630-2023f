7df387d5c9e31e938ce8b1f8ed89db4ad89b2d81
Alex M. Maldonado <aalexmmaldonado@gmail.com>
2023-08-20 19:14:52 -0400

    Adding some template materials

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@
 
 jupyter-book clean ./book
 # jupyter-book build ./book --builder linkcheck
+
+# Ensure we have all files possible
+export SECRETS_GPG_ARMOR=0
+git secret hide 2>&1 || true
+git secret reveal -fF 2>&1 || true
+
 jupyter-book build ./book
 ./cleanup_printing.py
 cp -rf ./book/slides/revealjs ./book/_build/html/slides/
